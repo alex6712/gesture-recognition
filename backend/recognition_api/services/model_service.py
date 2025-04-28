0a42f0839affa40deac3ec7468159ebdedc30500
alex6712 <alexeivanyukov@yandex.ru>
2025-04-28 20:16:21 +0300

    Небольшой рефакторинг. Дополнен README.md

--- backend/recognition_api/services/model_service.py
+++ backend/recognition_api/services/model_service.py
@@ -1,4 +1,4 @@
-from fastapi import status
+from fastapi import status, HTTPException
 from grpc import RpcError, StatusCode
 
 from repositories.model_repository import RecognitionModelRepository
@@ -6,8 +6,8 @@ from schemas.v1.responses import StandardResponse
 
 
 class RecognitionModelService:
-    def __init__(self, recognition_model_repo: RecognitionModelRepository):
-        self.model_repo: RecognitionModelRepository = recognition_model_repo
+    def __init__(self, model_repo: RecognitionModelRepository):
+        self.model_repo: RecognitionModelRepository = model_repo
 
     async def health_check(self):
         try:
@@ -16,11 +16,13 @@ class RecognitionModelService:
             return StandardResponse(message=response.message)
         except RpcError as e:
             if e.args[0].code == StatusCode.UNAVAILABLE:
-                return StandardResponse(
-                    code=status.HTTP_503_SERVICE_UNAVAILABLE,
-                    message="Соединение с моделью не установлено.",
+                raise HTTPException(
+                    status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+                    detail="Соединение с моделью не установлено.",
+                    headers={"Retry-After": "120"},
                 )
 
-        return StandardResponse(
-            code=status.HTTP_500_INTERNAL_SERVER_ERROR, message="Неизвестная ошибка."
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Неизвестная ошибка.",
         )
