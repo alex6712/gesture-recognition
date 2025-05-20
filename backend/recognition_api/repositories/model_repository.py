e69332a81a7db0a1cb7f7f1cc7ed5e1023d4c168
alex6712 <alexeivanyukov@yandex.ru>
2025-05-20 11:49:49 +0300

    Небольшой рефакторинг

--- backend/recognition_api/repositories/model_repository.py
+++ backend/recognition_api/repositories/model_repository.py
@@ -2,8 +2,8 @@ from interservice_grpc.stubs.model_stub import RecognitionModelStub
 
 
 class RecognitionModelRepository:
-    def __init__(self, grpc_client: RecognitionModelStub):
-        self.grpc_client = grpc_client
+    def __init__(self, grpc_stub: RecognitionModelStub):
+        self.grpc_stub: RecognitionModelStub = grpc_stub
 
     async def health_check(self):
-        return await self.grpc_client.health_check()
+        return await self.grpc_stub.health_check()
