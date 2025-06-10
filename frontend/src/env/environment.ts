1bacad0126758fdfc87165351ba6793ca50fd188
alex6712 <alexeivanyukov@yandex.ru>
2025-06-10 14:56:20 +0300

    Изменён базовый путь к API

--- frontend/src/env/environment.ts
+++ frontend/src/env/environment.ts
@@ -1,5 +1,5 @@
 import { EnvironmentModel } from './environment.model';
 
 export const environment: EnvironmentModel = {
-    APIBaseUrl: 'https://api.gesture-recognition-nntu.ru/api/v1',
+    APIBaseUrl: 'https://api.gesture-recognition-nntu.ru/v1',
 };
